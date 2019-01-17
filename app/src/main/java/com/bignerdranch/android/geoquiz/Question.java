37ed2df2c02a20fdf4bdece695a84d2e51ee0f76
Calvin Rai <raicalvin@gmail.com>
2019-01-17 09:52:02 -0800

    Created getter and setters for both fields in Question class.

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -10,4 +10,19 @@ public class Question {
         mAnswerTrue = answerTrue;
     }
 
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
 }
